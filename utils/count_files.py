eef053113dd3d6cb2bc89c68209a31e7b4802e8d
chaerin.dev <chaerin.dev@gmail.com>
2022-03-30 22:05:15 +0900

    Update count_files.py

--- utils/count_files.py
+++ utils/count_files.py
@@ -42,5 +42,5 @@ def update_readme():
 
 if __name__ == "__main__":
     readme = update_readme()
-    with open("./README.md", 'w', encoding='utf-8') as f:
+    with open("README.md", 'w', encoding='utf-8') as f:
         f.write(readme)
